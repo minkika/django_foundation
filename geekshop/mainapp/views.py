c8ccd83973fa19af3a6ea4a9188398a6e1b4815c
Lyubov Golubeva <golubeva.lyubov@gmail.com>
2020-08-02 19:54:27 +0300

    lesson 03

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,95 +1,32 @@
 import json
 
 from django.shortcuts import render
+from mainapp.models import Product
+from mainapp.models import ProductCategory
 
 
 def main(request):
     context = {
         'copyright': 'Golubeva Lyubov - GB',
         'title': 'geekshop',
+        'products': Product.objects.all()[:4]
     }
     return render(request, 'mainapp/index.html', context)
 
 
-def catalog_all(request):
-    links_menu = [
-        {'href': 'catalog_all', 'name': 'all'},
-        {'href': 'catalog_home', 'name': 'home'},
-        {'href': 'catalog_office', 'name': 'office'},
-        {'href': 'catalog_modern', 'name': 'modern'},
-        {'href': 'catalog_classic', 'name': 'classic'},
-    ]
+def catalog(request, pk=None):
+    title = 'All furniture'
+    if pk:
+        current_category = ProductCategory.objects.get(pk=pk)
+        title = current_category.name
+    links_menu = ProductCategory.objects.all()
+
     context = {
         'copyright': 'Golubeva Lyubov - GB',
-        'title': 'All furniture',
+        'title': title,
         'links_menu': links_menu,
     }
-    return render(request, 'mainapp/catalog_all.html', context)
-
-
-def catalog_home(request):
-    links_menu = [
-        {'href': 'catalog_all', 'name': 'all'},
-        {'href': 'catalog_home', 'name': 'home'},
-        {'href': 'catalog_office', 'name': 'office'},
-        {'href': 'catalog_modern', 'name': 'modern'},
-        {'href': 'catalog_classic', 'name': 'classic'},
-    ]
-    context = {
-        'copyright': 'Golubeva Lyubov - GB',
-        'title': 'Home furniture',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/catalog_all.html', context)
-
-
-def catalog_office(request):
-    links_menu = [
-        {'href': 'catalog_all', 'name': 'all'},
-        {'href': 'catalog_home', 'name': 'home'},
-        {'href': 'catalog_office', 'name': 'office'},
-        {'href': 'catalog_modern', 'name': 'modern'},
-        {'href': 'catalog_classic', 'name': 'classic'},
-    ]
-    context = {
-        'copyright': 'Golubeva Lyubov - GB',
-        'title': 'Office furniture',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/catalog_all.html', context)
-
-
-def catalog_modern(request):
-    links_menu = [
-        {'href': 'catalog_all', 'name': 'all'},
-        {'href': 'catalog_home', 'name': 'home'},
-        {'href': 'catalog_office', 'name': 'office'},
-        {'href': 'catalog_modern', 'name': 'modern'},
-        {'href': 'catalog_classic', 'name': 'classic'},
-    ]
-    context = {
-        'copyright': 'Golubeva Lyubov - GB',
-        'title': 'Modern furniture',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/catalog_all.html', context)
-
-
-def catalog_classic(request):
-    links_menu = [
-        {'href': 'catalog_all', 'name': 'all'},
-        {'href': 'catalog_home', 'name': 'home'},
-        {'href': 'catalog_office', 'name': 'office'},
-        {'href': 'catalog_modern', 'name': 'modern'},
-        {'href': 'catalog_classic', 'name': 'classic'},
-    ]
-    context = {
-        'copyright': 'Golubeva Lyubov - GB',
-        'title': 'Classic furniture',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/catalog_all.html', context)
-
+    return render(request, 'mainapp/catalog.html', context)
 
 def contacts(request):
     with open('contact_list.json') as f:
