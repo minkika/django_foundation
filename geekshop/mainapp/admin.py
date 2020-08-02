35c2a8fe0749294e0756e785a730826e475d72b7
Lyubov Golubeva <golubeva.lyubov@gmail.com>
2020-08-02 19:54:27 +0300

    lesson 03 not yet ready

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from mainapp.models import Product, ProductCategory
 
-# Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(Product)
--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=64, unique=True)
+    description = models.TextField(verbose_name='Description', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Name', max_length=128)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    short_desc = models.CharField(verbose_name='Short description', max_length=60, blank=True)
+    description = models.TextField(verbose_name='Description', blank=True)
+    price = models.DecimalField(verbose_name='Price', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveSmallIntegerField(verbose_name='In stock', default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
